d3235893fa44574451c276238dc38a4a3a253889
Parth Navapariya <145651514+ParthNavapariya@users.noreply.github.com>
2026-03-26 16:00:06 +0530

    Add files via upload

---
+++ ASSIGNMENT/SOFTWARE-ENGINEERING/MODULE-1/LAB-TASK/LAB-TASK_1.c
@@ -0,0 +1,7 @@
+// Write a simple "Hello World" program in two different programming languages of
+// your choice. Compare the structure and syntax.
+
+#include<stdio.h>
+int main(){
+    printf("hello world");
+}
