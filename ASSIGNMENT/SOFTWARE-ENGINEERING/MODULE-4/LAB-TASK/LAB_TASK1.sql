413431b944b7409633db0e63f1257a444a3154f6
Parth Navapariya <145651514+ParthNavapariya@users.noreply.github.com>
2026-05-01 12:42:35 +0530

    Add files via upload

---
+++ ASSIGNMENT/SOFTWARE-ENGINEERING/MODULE-4/LAB-TASK/LAB_TASK1.sql
@@ -0,0 +1,12 @@
+-- Create a new database named school_db and a table called students with the
+-- following columns: student_id, student_name, age, class, and address
+
+-- create database
+CREATE DATABASE SCHOOL_DB
+
+-- use databse
+USE SCHOOL_db
+
+
+--  create table
+CREATE TABLE STUDENT (STUDENT_ID int ,STUDENT_NAME varchar(20),AGE int, ADDRESS varchar(30));
