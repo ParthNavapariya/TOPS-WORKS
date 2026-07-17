089609e10fc231f88b88516b9fb0d8df2a4e07fc
Parth Navapariya <145651514+ParthNavapariya@users.noreply.github.com>
2026-07-17 14:29:50 +0530

    Delete ASSIGNMENT/SOFTWARE-ENGINEERING/MODULE-6/SESSION_6.py directory

--- ASSIGNMENT/SOFTWARE-ENGINEERING/MODULE-6/SESSION_6.py/Ipl.py
+++
@@ -1,9 +0,0 @@
-# Suppose you have two lists: one with IPL team names and another with their total points this season. Use zip() to combine them into a dictionary, then print only the teams that have more than 10 points.<br><br><em><strong>Hint:</strong> After creating the dictionary, use a for loop to filter and print.</em>
-
-ipl = ["mi","gt","csk"]
-point = [1,23,34]
-
-final = dict(zip(ipl,point))
-for key,value in final.items():
-        if value>10:
-                print(key,value)
--- ASSIGNMENT/SOFTWARE-ENGINEERING/MODULE-6/SESSION_6.py/list.py
+++
@@ -1,6 +0,0 @@
-# Given two lists — one of product names and one of their prices — use the zip() function to create a dictionary mapping each product to its price, then print the dictionary.
-
-product = ["mobile","headphone","laptop"]
-price = [100000,10000,150000]
-final = dict(zip(product,price))
-print(final)
